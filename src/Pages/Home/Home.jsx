55d463f3cca163cd09068a04038eb6ae47abf7be
Rasibul <rasibul179@gmail.com>
2023-11-15 00:08:04 +0600

    bistro-boss-resturent nav,banner and footer section create

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "./Banner/Banner";
+import Categeroy from "./Categeroy/Categeroy";
+
+
+
+const Home = () => {
+    return (
+        <div>
+           <Banner></Banner>
+           <Categeroy></Categeroy>
+        </div>
+    );
+};
+
+export default Home;
