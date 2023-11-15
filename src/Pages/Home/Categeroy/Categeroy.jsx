f3f079f40dede82d90d5ceed1248657d1b57fe4a
Rasibul <rasibul179@gmail.com>
2023-11-15 09:36:56 +0600

    Added a section title in home page anfmake section title component

--- src/Pages/Home/Categeroy/Categeroy.jsx
+++ src/Pages/Home/Categeroy/Categeroy.jsx
@@ -1,20 +1,22 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-// import required modules
-
-
 import slide1 from '../../../assets/home/slide1.jpg'
 import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../Component/SectionTitle';
+
 
 const Categeroy = () => {
     return (
-        <>
+        <section>
+             <SectionTitle
+            subHeading={"From 11.00am to 10.00pm"}
+            heading={"Order Online"}
+            ></SectionTitle>
             <Swiper
                 slidesPerView={4}
                 spaceBetween={30}
@@ -45,10 +47,8 @@ const Categeroy = () => {
                     <img src={slide5} alt="" />
                     <h3 className="text-3xl text-center uppercase text-white -mt-16">Salad</h3>
                 </SwiperSlide>
-               
-               
             </Swiper>
-        </>
+        </section>
     );
 };
 
