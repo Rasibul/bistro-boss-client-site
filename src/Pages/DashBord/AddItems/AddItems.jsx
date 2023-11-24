0088d25609ce1b0674520e50edee903f1daed731
Rasibul <rasibul179@gmail.com>
2023-11-24 06:57:17 +0600

    food item add by admin from item food from

--- src/Pages/DashBord/AddItems/AddItems.jsx
+++ src/Pages/DashBord/AddItems/AddItems.jsx
@@ -1,14 +1,49 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Component/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from '../../../Hooks/useAxiosPublic'
+import useAxioSecure from '../../../Hooks/useAxiosSecure'
+import Swal from "sweetalert2";
 
-
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 const AddItems = () => {
-    const { register, handleSubmit } = useForm()
-    const onSubmit = (data) => {
-        
+    const { register, handleSubmit,reset } = useForm()
+    const axiosPublic = useAxiosPublic()
+    const axiosSecure = useAxioSecure()
+    const onSubmit = async (data) => {
         console.log(data)
-        
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+        if(res.data.success){
+                // now send the menu item data to the server with the image url
+                const menuItem = {
+                    name: data.name,
+                    category: data.category,
+                    price: parseFloat(data.price),
+                    recipe: data.recipe,
+                    image: res.data.data.display_url
+                }
+                const menuRes = await axiosSecure.post('/api/v1/menus',menuItem)
+                console.log(menuRes.data)
+                if(menuRes.data.insertedId){
+                    // show success popup
+                    reset();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${data.name} is added to the menu.`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                }
+        }
+        console.log( 'with image url', res.data);
+
     }
     return (
         <div>
